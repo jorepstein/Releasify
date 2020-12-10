dc9df4023853e78406ebd3063247d9a0eb8adfe8
Jordan Epstein <epstein.jor@gmail.com>
2020-12-10 14:41:40 -0500

    Dont add repeat songs, add songs in batches

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import argparse
+from collections import defaultdict
 import spotipy
 from spotipy.oauth2 import SpotifyOAuth, SpotifyClientCredentials
 import time
@@ -23,6 +24,7 @@ class Releasify:
                 client_id=CLIENT_ID,
                 client_secret=CLIENT_SECRET,
             ))
+        self._added_tracks = defaultdict(list)
 
     @property
     def user_id(self):
@@ -46,24 +48,14 @@ class Releasify:
 
     def _process_playlist(self, playlist_id_src, playlist_id_dest: str,
                           time_window_days: int):
-        all_tracks = set()
-
         artist_ids = self._get_artist_ids_from_playlist_id(playlist_id_src)
-        print(f"Found {len(artist_ids)} artists on playlist.")
+        print(f"Found {len(artist_ids)} artists on playlist ({self._get_playlist_name(playlist_id_src)})")
         for artist_id in list(artist_ids):
             album_ids = self._get_album_ids_from_artist_id(
                 artist_id, time_window_days)
             for album_id in album_ids:
                 track_ids = self._get_track_ids_from_album_id(album_id)
-                track_ids = [
-                    track_id for track_id in track_ids
-                    if track_id not in all_tracks
-                ]
-                if track_ids:
-                    self._user_spotify.user_playlist_add_tracks(
-                        self.user_id, playlist_id_dest, track_ids)
-                all_tracks.update(track_ids)
-        print(f"Found {len(all_tracks)} new tracks.")
+                self._add_tracks_to_playlist(track_ids, playlist_id_dest)
 
     def _create_new_playlist(self, base_name: str) -> str:
         new_name = f'Releasify: {base_name}'
@@ -78,6 +70,21 @@ class Releasify:
         playlist_name = self._user_spotify.playlist(playlist_id,
                                                     fields='name')['name']
         return playlist_name
+
+    def _add_tracks_to_playlist(self, track_ids, playlist_id):
+        to_add = []
+        for track_id in track_ids:
+            if track_id not in self._added_tracks[playlist_id]:
+                to_add.append(track_id)
+        if to_add:
+            limit = 100
+            num_batches = len(to_add) // limit + 1
+            for batch_num in range(num_batches):
+                self._user_spotify.user_playlist_add_tracks(
+                    self.user_id, playlist_id,
+                    to_add[batch_num * limit:(batch_num + 1) * limit])
+        self._added_tracks[playlist_id].extend(to_add)
+        print(f"Added {len(to_add)} new tracks.")
 
     def _get_artist_ids_from_playlist_id(self, playlist_id: str) -> List[str]:
         limit = 100
@@ -106,14 +113,13 @@ class Releasify:
         num_batches = num_albums // limit + 1
         for i in range(num_batches):
             results = self._client_spotify.artist_albums(
-                artist_id, limit=limit, offset=i * limit,
-                album_type='album,single')
+                artist_id, limit=limit, offset=i * limit)
             for album in results['items']:
                 release_time = get_release_time(album['release_date'],
                                                 album['release_date_precision'])
                 time_window_seconds = time_window_days * 24 * 60 * 60
                 seconds_since_release = get_current_time() - release_time
-                if release_time and seconds_since_release < time_window_seconds:
+                if seconds_since_release < time_window_seconds:
                     all_album_ids.add(album['id'])
         return all_album_ids
 
@@ -142,7 +148,7 @@ def get_release_time(release_date: str, precision: str) -> int:
     try:
         release_time = int(time.mktime(time.strptime(release_date, pattern)))
     except OverflowError:
-        return None
+        return 0
     return release_time
 
 
